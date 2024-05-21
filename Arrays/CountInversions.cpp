9efacbba5881d509615f9f78595686647a3a4d7a
FedoraSukant <guptasukant2@gmail.com>
2024-05-21 12:58:00 +0530

    Minor Changes

--- Arrays/CountInversions.cpp
+++ Arrays/CountInversions.cpp
@@ -1,4 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// $ 
+// $ Count the number of inversions in an array
+// $ Inversion is a pair of indices i, j such that i < j and a[i] > a[j]
+
+// $ Brute Force
