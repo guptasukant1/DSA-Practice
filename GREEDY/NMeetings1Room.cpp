3bcb24bed5191ef30f58e5b136a44caa906ed2bf
FedoraSukant <guptasukant2@gmail.com>
2024-05-23 00:35:41 +0530

    Added Greedy ques n meetings in a room

---
+++ GREEDY/NMeetings1Room.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define MOD 1000000007
+#define INF 1e9
+#define N 100000
+#define deb(x) cout << #x << " " << x << endl;
+#define print(v) for(auto x: v) cout << x << " "; cout << endl;
+
+
+
+
+int main(){
+
+}
