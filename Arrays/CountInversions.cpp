b1f083603f68dfecf6c5930e67b3be1583f69ca2
FedoraSukant <guptasukant2@gmail.com>
2024-05-26 00:44:57 +0530

    Added Optimal Sol to count inversions

--- Arrays/CountInversions.cpp
+++ Arrays/CountInversions.cpp
@@ -18,7 +18,40 @@ int countInversions(vi a){
 
 // $ Optimal Approach[Merge Sort]
 
+int merge(vi a, int low, int mid, int high){
+    vi temp;
+    int left = low;
+    int right = mid + 1;
 
+    int count = 0;
+
+    while(left <= mid && right <= high){
+        if(a[left] <= a[right]){
+            temp.push_back(a[left]);
+            left++;
+        }
+        else{
+            temp.push_back(a[right]);
+            count += (mid - left + 1);
+            right++;
+        }
+    }
+
+    while(left <= mid){
+        temp.push_back(a[left]);
+        left++;
+    }
+
+    while(right <= high){
+        temp.push_back(a[right]);
+        right++;
+    }
+
+    for(int i = low; i < high; i++){
+        a[i] = temp[i - low];
+    }
+    return count;
+}
 
 int main(){
     vector<int> a = {5, 4, 3, 2, 1};
