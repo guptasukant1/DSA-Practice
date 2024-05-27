242625096c3e9e1dc2ec36f0f73f4527d295a290
FedoraSukant <guptasukant2@gmail.com>
2024-05-28 00:05:52 +0530

    Added Search in a 2D sorted matrix better approach

--- Arrays/SearchSorted2D.cpp
+++ Arrays/SearchSorted2D.cpp
@@ -1,11 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define vvi vector<vector<int>>
-
+#define vi vector<int>
 
 // $ Search an element in a 2D sorted array
 
 // $ Brute-Force Approach
+// $ TC: O(n*m) | SC: O(1)
 bool searchMat(vvi &matrix, int target){
     int n = matrix.size(), m = matrix[0].size();
 
@@ -17,8 +18,33 @@ bool searchMat(vvi &matrix, int target){
     return false;
 }
 
+// $ Better Approach[Binary Search - selective search - check the row where the target may be and then apply binary search there]
+// $ TC: O() | SC: O()
+bool BinarySearch(vi &nums, int target){
+    int n = nums.size();
+    int low = 0, high = n - 1;
+
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(nums[mid] == target) return true;
+        else if(target < nums[mid]) high = mid - 1;
+        else low = mid + 1;
+    }
+    return false;
+}
+bool searchMatBe(vvi &matrix, int target){
+    int n = matrix.size(), m = matrix[0].size();
+
+    for(int i = 0; i < n; i++){
+        if(target >= matrix[i][0] && target < matrix[i][m - 1]) return BinarySearch(matrix[i], target);
+    }
+    return false;
+}
+
 
 int main(){
-    vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    vvi matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
     searchMat(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+    searchMatBe(matrix, 13) == true ? cout << "true\n" : cout << "false\n";
+
 }
