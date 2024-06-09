df6deaa63df10e0f630fc40e5533b9e6259f71a5
FedoraSukant <guptasukant2@gmail.com>
2024-06-09 17:58:51 +0530

    Added Better approach of majority element

--- Arrays/MajorityElement.cpp
+++ Arrays/MajorityElement.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define vi vector<int>
+#define miip map<int, int>
 
 // $ Majority Element in an array is an element that appears more than n/2 times in the array
 
@@ -17,10 +18,28 @@ int majorityElement(vi v){
     return -1;
 }
 
+// $ Better Approach
+int majorityElementBe(vi v){
+    int n = v.size();
+    miip mpp;
+    
+    for(int i = 0; i < n; i++){
+        mpp[v[i]]++;
+    }
+
+    for(auto i: mpp){
+        if(i.second > (n / 2)) return i.first;
+    }
+    return -1;
+}
+
+
 int main()
 {
-    vi arr = {2, 2, 1, 1, 1, 2, 3};
+    vi arr = {2, 2, 1, 1, 1, 2, 2};
     int ans = majorityElement(arr);
     cout << "The majority element is: " << ans << endl;
-    return 0;
+    vi arr1 = {4, 4, 5, 6, 7, 4, 4, 4};
+    int ans1 = majorityElementBe(arr1);
+    cout << "The majority element is: " << ans1 << endl;
 }
