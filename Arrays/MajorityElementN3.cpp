f98615225c228e31180008e26f8b76848d64b2bf
FedoraSukant <guptasukant2@gmail.com>
2024-06-20 16:51:50 +0530

    added majelem n/

---
+++ Arrays/MajorityElementN3.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define miip map<int, int>
+#define ll long long
+#define vii vector<vector<int>>
+
+// $ Majority Element in an array is an element that appears more than n/3 times in the array
+
+// $ Brute-Force Approach
