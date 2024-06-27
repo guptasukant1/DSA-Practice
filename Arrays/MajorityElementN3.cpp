8c4f300bd9b466446cd3420f2ea174896bf1fa8e
FedoraSukant <guptasukant2@gmail.com>
2024-06-27 22:10:18 +0530

    majority elem n/3 oa sol

--- Arrays/MajorityElementN3.cpp
+++ Arrays/MajorityElementN3.cpp
@@ -45,7 +45,41 @@ vi majelemBe(vi v){
 // $ Optimal Approach [Modified Moore's voting algo]
 // $ TC: O() | SC: O()
 vi majelemOA(vi v){
-    
+    int n = v.size();
+    int cnt1 = 0, cnt2 = 0;
+    int el1 = INT_MIN;
+    int el2 = INT_MIN;
+
+    for(int i = 0; i < n; i++){
+        if(cnt1 == 0 && el2 != v[i]){
+            cnt1 = 1;
+            el1 = v[i];
+        }
+        else if(cnt2 == 0 && el1 != v[i]){
+            cnt2 = 1;
+            el2 = v[i];
+        }
+        else if(v[i] == el1) cnt1++;
+        else if(v[i] == el2) cnt2++;
+        else cnt1--, cnt2--;
+    }
+
+    vi ts;
+
+    cnt1 = 0, cnt2 = 0;
+    for(int i = 0; i < n; i++){
+        if(v[i] == el1) cnt1++;
+        if(v[i] == el2) cnt2++;
+    }
+
+    int mini = int(n / 3) + 1;
+    if(cnt1 >= mini) ts.push_back(el1);
+    if(cnt2 >= mini) ts.push_back(el2);
+
+    // sort(ts.begin(), ts.end());
+    // * TC: O(2log2) -> ~ O(1)
+
+    return ts;
 }
 
 
@@ -62,5 +96,10 @@ int main(){
     for (auto it : ans1)
         cout << it << " ";
     cout << "\n";
-    return 0;
+    vi arr2 = {10, 20, 40, 20, 40, 20, 10, 10, 40, 40, 10};
+    vi ans2 = majelemOA(arr2);
+    cout << "The majority elements are: ";
+    for (auto it : ans2)
+        cout << it << " ";
+    cout << "\n";
 }
