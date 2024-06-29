d42f4e14b37c22addcbc18d43cfad8ea450a27fa
FedoraSukant <guptasukant2@gmail.com>
2024-06-29 14:08:28 +0530

    added OA sol to unique grid paths

--- Arrays/GridPaths.cpp
+++ Arrays/GridPaths.cpp
@@ -20,21 +20,31 @@ public:
         return count_paths(0, 0, n, m);
     }
 
-    // $ Better Approach
-    // $ TC: O() | SC: O()
+    // $ Better Approach [DP]
+    // $ TC: O(n*m) | SC: O(n*m)
     int count_pathsBe(int i, int j, int n, int m, vvi &dp){
         if(i == n - 1 and j == m - 1) return 1;
         if(i >= n || j >= m) return 0;
         if(dp[i][j] != -1) return dp[i][j];
         else return dp[i][j] = count_pathsBe(i + 1, j, n, m, dp) + count_pathsBe(i, j + 1, n, m, dp);
     }
-
     int uniquePathsBe(int n, int m){
         vvi dp(n + 1, vi(m + 1, -1));
         int num = count_pathsBe(0, 0, n, m, dp);
         // * dp[1][1] = 1
         if(m == 1 && n == 1) return num;
         return dp[0][0];
+    }
+
+    // $ Optimal Approach [Combinatorics]
+    // $ TC: O() | SC: O()
+    int uniquePathsOA(int n, int m){
+        int N = n + m - 2;
+        int r = n - 1;
+        int res = 1;
+
+        for(int i = 1; i <= r; i++) res = res * (N - r + i) / i;
+        return (int)res;
     }
 };
 
@@ -45,4 +55,8 @@ int main(){
     cout<<"The total number of Unique Paths are "<<totalCount<<endl;
     int totalCount1= o1.uniquePathsBe(4,7);
     cout<<"The total number of Unique Paths are "<<totalCount1<<endl;
+    Solution o2;
+    int totalCount2 = o1.uniquePathsOA(4,7);
+    cout<<"The total number of Unique Paths are "<< totalCount2 <<endl;
+    
 }
