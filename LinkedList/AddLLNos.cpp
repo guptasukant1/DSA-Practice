3b3bb532e792793d22edfc0af00e1baebbc469b3
FedoraSukant <guptasukant2@gmail.com>
2024-07-22 01:33:03 +0530

    added oa sol for add nos in ll form

---
+++ LinkedList/AddLLNos.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// $ Add 2 Nos in the form of LL
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+    Node(){
+        data = 0;
+        next = nullptr;
+    }
+};
+
+
+// $ Optimal Approach
+// $ TC: O(max(m, n)) | SC: O(max(m, n))
+class Solution{
+public:
+    Node* addNos(Node* l1, Node* l2){
+        Node* dum = new Node();
+        Node* temp = dum;
+
+        int carry = 0;
+        while( (l1 != nullptr || l2 != nullptr) || carry){
+            int sum = 0;
+            if(l1 != nullptr){
+                sum += l1 -> data;
+                l1 = l1 -> next;
+            }
+
+            if(l2 != nullptr){
+                sum += l2 -> data;
+                l2 = l2 -> next;
+            }
+
+            sum += carry;
+            carry = sum / 10;
+            Node* newNode = new Node(sum % 10);
+            temp -> next = newNode;
+            temp = temp -> next;
+        }
+        return dum -> next;
+    }
+};
