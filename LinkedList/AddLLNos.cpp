814b4bd0ab7984e43d9f9d7ad517cd0594de35f0
FedoraSukant <guptasukant2@gmail.com>
2024-07-23 00:28:59 +0530

    removed cond on add ll nos

--- LinkedList/AddLLNos.cpp
+++ LinkedList/AddLLNos.cpp
@@ -33,22 +33,21 @@ public:
         Node* temp = dum;
 
         int carry = 0;
-        while( (l1 != nullptr || l2 != nullptr) || carry){
+        while( (l1 || l2) || carry){
             int sum = 0;
-            if(l1 != nullptr){
+            if(l1){
                 sum += l1 -> data;
                 l1 = l1 -> next;
             }
 
-            if(l2 != nullptr){
+            if(l2){
                 sum += l2 -> data;
                 l2 = l2 -> next;
             }
 
             sum += carry;
             carry = sum / 10;
-            Node* newNode = new Node(sum % 10);
-            temp -> next = newNode;
+            temp -> next = new Node(sum % 10);
             temp = temp -> next;
         }
         return dum -> next;
