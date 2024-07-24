58a42489313481d293a32f7d32ac03cbfd1bd610
FedoraSukant <guptasukant2@gmail.com>
2024-07-25 00:40:54 +0530

    added bf of intersection ll

--- LinkedList/IntersectionLL.cpp
+++ LinkedList/IntersectionLL.cpp
@@ -27,3 +27,47 @@ void insertNode(node* &head,int val) {
 
 // $ Brute Force Approach
 // $ TC: O() | SC: O()
+node* IntersectionBF(node* head1, node* head2){
+    while(head2 != nullptr){
+        node* temp = head1;
+        while(temp != nullptr){
+            if(temp == head2) return head2;
+            temp = temp -> next;
+        }
+        head2 = head2 -> next;
+    }
+    return nullptr;
+}
+
+
+void printList(node* head) {
+    while(head->next != NULL) {
+        cout<<head->data<<"->";
+        head = head->next;
+    }
+    cout<<head->data<<endl;
+}
+
+int main() {
+    node* head = NULL;
+    insertNode(head,1);
+    insertNode(head,3);
+    insertNode(head,1);
+    insertNode(head,2);
+    insertNode(head,4);
+    node* head1 = head;
+    head = head->next->next->next;
+    node* headSec = NULL;
+    insertNode(headSec,3);
+    node* head2 = headSec;
+    headSec->next = head;
+
+    cout<<"List1: "; printList(head1);
+    cout<<"List2: "; printList(head2);
+
+    node* answerNode = IntersectionBF(head1,head2);
+    if(answerNode == NULL )
+    cout<<"No intersection\n";
+    else
+    cout<<"The intersection point is "<<answerNode->data<<endl;
+}
