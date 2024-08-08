c7800b48e32243a3121954211d91d5c2235bd943
FedoraSukant <guptasukant2@gmail.com>
2024-08-08 23:25:12 +0530

    added bfa for 3 sum using 3 loops and set to store the temp arrs of each 3 elems

--- Arrays/3sum.cpp
+++ Arrays/3sum.cpp
@@ -1,5 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define vvi vector<vector<int>>
+#define svi set<vector<int>>
+#define vi vector<int>
 
 // $ Triplets adding up to zero
 
+vvi triplet(int n, vi &arr){
+    svi st;
+
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            for (int k = j + 1; k < n; k++){
+                if(arr[i] + arr[j] + arr[k] == 0){
+                    vi temp = {arr[i], arr[j], arr[k]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+            }
+            
+        }
+    }
+    vvi ans(st.begin(), st.end());
+    return ans;
+}
+
+
+
+
+int main()
+{
+    vector<int> arr = { -1, 0, 1, 2, -1, -4};
+    int n = arr.size();
+    vector<vector<int>> ans = triplet(n, arr);
+    for (auto it : ans) {
+        cout << "[";
+        for (auto i : it) {
+            cout << i << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+    return 0;
+}
