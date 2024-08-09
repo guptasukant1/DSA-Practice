79e35c7aec5fdbd53906f4b49dc9840c748b6c91
FedoraSukant <guptasukant2@gmail.com>
2024-08-09 23:57:44 +0530

    added oa for 3sum using 3 pointer approach which checks whether the sum is 0 or not by checking the elems in sorted array

--- Arrays/3sum.cpp
+++ Arrays/3sum.cpp
@@ -7,7 +7,7 @@ using namespace std;
 // $ Triplets adding up to zero
 
 // $ Brute Force Approach
-// $ TC: O(n^3 * no. of unique triplets) | SC: O(2 * no. of unique triplets)
+// $ TC: O(n^3 * log(no. of unique triplets)) | SC: O(2 * no. of unique triplets)
 vvi tripletBF(int n, vi &arr){
     svi st;
 
@@ -28,7 +28,7 @@ vvi tripletBF(int n, vi &arr){
 }
 
 // $ Better Approach [using hashset to store the elements between i & j and finding the third elem in it using the formula.]
-// $ TC: O() | SC: O()
+// $ TC: O(n ^ 2 * log(no. of unique triplets)) | SC: O(2 * no. of unique triplets)
 vvi tripletBeA(int n, vi &arr){
     svi st;
 
@@ -48,6 +48,32 @@ vvi tripletBeA(int n, vi &arr){
     return ans;
 }
 
+// $ Optimal Approach
+// $ TC: O() | SC: O()
+vvi tripletOA(int n, vi &arr){
+    vvi ans;
+    sort(arr.begin(), arr.end());
+    for(int i = 0; i < n; i++){
+        if(i != 0 && arr[i - 1] == arr[i]) continue;
+        int j = i + 1;
+        int k = n - 1;
+        while(j < k){
+            int sum = arr[i] + arr[j] + arr[k];
+            if(sum > 0) k--;
+            else if(sum < 0) j++;
+            else {
+                vi temp = {arr[i], arr[j], arr[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                while(j < k && arr[j] == arr[j - 1]) j++;
+                while(j < k && arr[k] == arr[k + 1]) k--;
+            }
+        }
+    }
+    return ans;
+}
+
 
 int main()
 {
@@ -55,6 +81,26 @@ int main()
     int n = arr.size();
     vvi ans = tripletBF(n, arr);
     for (auto it : ans) {
+        cout << "[";
+        for (auto i : it) {
+            cout << i << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+
+    vvi ans1 = tripletBeA(n, arr);
+    for (auto it : ans1) {
+        cout << "[";
+        for (auto i : it) {
+            cout << i << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+
+    vvi ans2 = tripletOA(n, arr);
+    for (auto it : ans2) {
         cout << "[";
         for (auto i : it) {
             cout << i << " ";
