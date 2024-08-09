317bef7db1819174c8ffc121f355c06854d34a1d
FedoraSukant <guptasukant2@gmail.com>
2024-08-09 20:00:36 +0530

    added better app for 3sum using hashset to store elems b/w i & j and finding the third elem in it using the formula

--- Arrays/3sum.cpp
+++ Arrays/3sum.cpp
@@ -6,7 +6,9 @@ using namespace std;
 
 // $ Triplets adding up to zero
 
-vvi triplet(int n, vi &arr){
+// $ Brute Force Approach
+// $ TC: O(n^3 * no. of unique triplets) | SC: O(2 * no. of unique triplets)
+vvi tripletBF(int n, vi &arr){
     svi st;
 
     for(int i = 0; i < n; i++){
@@ -25,14 +27,33 @@ vvi triplet(int n, vi &arr){
     return ans;
 }
 
+// $ Better Approach [using hashset to store the elements between i & j and finding the third elem in it using the formula.]
+// $ TC: O() | SC: O()
+vvi tripletBeA(int n, vi &arr){
+    svi st;
 
+    for(int i = 0; i < n; i++){
+        set<int> hashSet;
+        for(int j = i + 1; j < n; j++){
+            int third = -( arr[i] + arr[j] );
+            if(hashSet.find(third) != hashSet.end()){ // * Returns an iterator to the element if found else returns end()
+                vi temp = {arr[i], arr[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
+            }
+            hashSet.insert(arr[j]);
+        }
+    }
+    vvi ans(st.begin(), st.end());
+    return ans;
+}
 
 
 int main()
 {
-    vector<int> arr = { -1, 0, 1, 2, -1, -4};
+    vi arr = { -1, 0, 1, 2, -1, -4};
     int n = arr.size();
-    vector<vector<int>> ans = triplet(n, arr);
+    vvi ans = tripletBF(n, arr);
     for (auto it : ans) {
         cout << "[";
         for (auto i : it) {
