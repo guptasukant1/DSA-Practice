c40333453faa4c4e31ae95d2b42cdcd5c3e09c99
FedoraSukant <guptasukant2@gmail.com>
2024-08-10 22:58:22 +0530

    added bfa and better app for trapping water using (max left and right at each index and calc the water trapped at the index) | (using prefix array to reduce the tc by calculatig the max for each index individually)

---
+++ Arrays/TrappingWater.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+
+// $ Trapping Water Problem
+
+// $ Brute Force Approach [finding the maximum left and right at each index and calculate the water trapped at that index]
+// $ TC: O(n ^ 2) | SC: O(1)
+int trap(vi &arr){
+    int n = arr.size();
+    int water = 0;
+    for(int i = 0; i < n; i++){
+        int j = i;
+        int leftMax = 0, rightMax = 0;
+        while(j >= 0){
+            leftMax = max(leftMax, arr[j]);
+            j--;
+        }
+        j = i;
+        while(j < n){
+            rightMax = max(rightMax, arr[j]);
+            j++;
+        }
+        water += min(leftMax, rightMax) - arr[i];
+    }
+    return water;
+}
+
+// $ Better Approach
+// $ TC: O() | SC: O()
+int trapBeA(vi &arr){
+    int n = arr.size();
+    int prefix[n], suffix[n];
+    // * Calculate prefix and suffix arrays by setting at start for prefix and at end for suffix
+    prefix[0] = arr[0];
+    for(int i = 1; i < n; i++) prefix[i] = max(prefix[i - 1], arr[i]);
+
+    suffix[n - 1] = arr[n - 1];
+    for(int i = n - 2; i >= 0; i--) suffix[i] = max(suffix[i + 1], arr[i]);
+
+    int water = 0;
+    for(int i = 0; i < n; i++) water += min(prefix[i], suffix[i]) - arr[i];
+    return water;
+}
+
+
+
+int main() {
+    vi arr;
+    arr = {0,1,0,2,1,0,1,3,2,1,2,1};
+    cout << "The water that can be trapped is " << trap(arr) << endl;
+    cout << "The water that can be trapped is " << trapBeA(arr) << endl;
+}
--- MISC/PascalTriangle.cpp
+++ MISC/PascalTriangle.cpp
@@ -5,8 +5,7 @@ using namespace std;
 // $ Pascal's Triangle
 
 // ? Variation 1 [row and col given]
-// $ TC: O() | SC: O()
-
+// $ TC: O(c) | SC: O(1)
 int ncr(int n, int r){
     ll res = 1;
     for(int i = 0; i < r; i++){
@@ -28,3 +27,5 @@ int main(){
     int element = pascalTriangle(r, c);
     cout << "The element at position (r,c) is: " << element;
 }
+
+// ? Variation 2 [print nth row of pascal's triangle]
