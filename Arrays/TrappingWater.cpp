ccb1a6f08523018745537e6e4e362910240356c6
FedoraSukant <guptasukant2@gmail.com>
2024-08-11 20:14:52 +0530

    added oa for trapping water using 2 pointer approach by finding max left & right to find the water trapped at l and r w.r.t current value and avoiding extra calc for l&r at each index

--- Arrays/TrappingWater.cpp
+++ Arrays/TrappingWater.cpp
@@ -26,7 +26,7 @@ int trap(vi &arr){
     return water;
 }
 
-// $ Better Approach
+// $ Better Approach [using prefix and suffix array to find the max left and right at each index and reduce tc by calculating them individually]
 // $ TC: O() | SC: O()
 int trapBeA(vi &arr){
     int n = arr.size();
@@ -34,7 +34,6 @@ int trapBeA(vi &arr){
     // * Calculate prefix and suffix arrays by setting at start for prefix and at end for suffix
     prefix[0] = arr[0];
     for(int i = 1; i < n; i++) prefix[i] = max(prefix[i - 1], arr[i]);
-
     suffix[n - 1] = arr[n - 1];
     for(int i = n - 2; i >= 0; i--) suffix[i] = max(suffix[i + 1], arr[i]);
 
@@ -44,10 +43,33 @@ int trapBeA(vi &arr){
 }
 
 
+// $ Optimal Approach [2 pointer approach -> set ptrs at ends, finding left and right max to to find whether water is trapped at the left or right end and adding to res]
+// $ [and avoiding separate calc of max left and right at each index]
+// $ TC: O(n) | SC: O(1)
+int trapOA(vi &arr){
+    int n = arr.size();
+    int left = 0, right = n - 1;
+    int res = 0, maxLeft = 0, maxRight = 0;
+    while(left <= right){
+        if(arr[left] <= arr[right]){
+            if(arr[left] >= maxLeft) maxLeft = arr[left];
+            else res += maxLeft - arr[left];
+            left++;
+        }
+        else{
+            if(arr[right] >= maxRight) maxRight = arr[right];
+            else res += maxRight - arr[right];
+            right--;
+        }
+    }
+    return res;
+}
+
 
 int main() {
     vi arr;
     arr = {0,1,0,2,1,0,1,3,2,1,2,1};
     cout << "The water that can be trapped is " << trap(arr) << endl;
     cout << "The water that can be trapped is " << trapBeA(arr) << endl;
+    cout << "The water that can be trapped is " << trapOA(arr) << endl;
 }
