1219461d0748bcda9bad6df4c2f93144065b9ccd
FedoraSukant <guptasukant2@gmail.com>
2024-08-13 23:28:43 +0530

    added n meetings 1 room using struct for s, e, pos and mapping to get the meetings that can be held by comparing the end times and meeting nos

--- GREEDY/NMeetings1Room.cpp
+++ GREEDY/NMeetings1Room.cpp
@@ -15,19 +15,16 @@ using namespace std;
 #define deb(x) cout << #x << " " << x << endl;
 #define print(v) for(auto x: v) cout << x << " "; cout << endl;
 
-
-
 // $ N meetings in one room
 // $ Given N meetings with their start and end time, we have to find the maximum number of meetings that can be held in one room.
 
-struct meeting{
-    int start, end, pos;
-};
+// struct meeting{
+//     int start, end, pos;
+// };
 
 // bool comparator(meeting m1, meeting m2){
 //     return m1.end < m2.end;
 // }
-
 // void maxMeetings(int start[], int end[], int n){
 //     meeting meet[n];
 //     for(int i=0; i<n; i++){
@@ -35,42 +32,39 @@ struct meeting{
 //         meet[i].end = end[i];
 //         meet[i].pos = i+1;
 //     }
-
 //     sort(meet, meet+n, comparator);
-
 //     vi ans;
 //     int limit = meet[0].end;
 //     ans.pb(meet[0].pos);
-
 //     for(int i=1; i<n; i++){
 //         if(meet[i].start > limit){
 //             limit = meet[i].end;
 //             ans.pb(meet[i].pos);
 //         }
 //     }
-
 //     print(ans);
 // }
 
 
-class Solution{
-    public:
-        bool static comparator(struct meeting m1, meeting m2){
-            if(m1.end < m2.end) return true;
-            else if(m1.end > m2.end) return false;
-            else if(m1.pos < m2.pos) return true;
-            return false;
-        }
-        void maxMeetings(int start[], int end[], int n){
-            struct meeting meet[n];
-            for (int i = 0; i < n; i++)
-            {
-                meet[i].start = start[i], meet[i].end = end[i], meet[i].pos = i + 1;
-            }
-            sort(meet, meet + n, comparator);
+// class Solution{
+//     public:
+//         bool static comparator(struct meeting m1, meeting m2){
+//             if(m1.end < m2.end) return true;
+//             else if(m1.end > m2.end) return false;
+//             else if(m1.pos < m2.pos) return true;
+//             return false;
+//         }
+//         void maxMeetings(int start[], int end[], int n){
+//             struct meeting meet[n];
+//             for (int i = 0; i < n; i++)
+//             {
+//                 meet[i].start = start[i], meet[i].end = end[i], meet[i].pos = i + 1;
+//             }
+//             sort(meet, meet + n, comparator);
+//         }
+// };
 
-        }
-};
+
 
 
 
---
+++ GREEDY/Nmeet1RoomNew.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+
+// $ N meetings 1 Room
+// $ TC: O(n + nlogn + n) | SC: O(n )
+struct meeting{
+    int start, end, pos;
+};
+
+class Solution{
+public:
+    bool static comparator(struct meeting m1, struct meeting m2){
+        if(m1.end < m2.end) return true;
+        else if(m1.end > m2.end) return false;
+        else if(m1.pos < m2.pos) return true;
+        return false;
+    }
+
+    void maxMeetings(int s[], int e[], int n){
+        struct meeting meet[n];
+        for(int i = 0; i < n; i++){
+            meet[i].start = s[i], meet[i].end = e[i], meet[i].pos = i + 1;
+        }
+        sort(meet, meet + n, comparator);
+
+        vi ans;
+        int limit = meet[0].end;
+        ans.push_back(meet[0].pos);
+
+        for(int i = 1; i < n; i++){
+            if(meet[i].start > limit){
+                limit = meet[i].end;
+                ans.push_back(meet[i].pos);
+            }
+        }
+        
+        cout << "Order: " << endl;
+        for(int i = 0; i < ans.size(); i++){
+            cout << ans[i] << " ";
+        }
+    }
+};
+
+
+int main() {
+   Solution obj;
+   int n = 6;
+   int start[] = {1,3,0,5,8,5};
+   int end[] = {2,4,5,7,9,9};
+   obj.maxMeetings(start, end, n);
+   return 0;
+}
