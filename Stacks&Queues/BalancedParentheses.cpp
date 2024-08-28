8c17c6ab7bbe8549e1a4ed60e38c415d251441c7
FedoraSukant <guptasukant2@gmail.com>
2024-08-28 21:02:10 +0530

    added balanced parentheses in S&Q using loop to compare the elems

---
+++ Stacks&Queues/BalancedParentheses.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// $ Check for Balanced Parentheses
+
+bool isValid(string s){
+    stack<char> st;
+    for(auto it: s){
+        if(it == '{' || it == '(' || it == '[') st.push(it);
+        else{
+            if(st.size() == 0) return false;
+            char ch = st.top();
+            st.pop();
+            if( (ch == '(' && it == ')') || (ch == '{' && it == '}') || (ch == '[' && it == ']')) continue;
+            else return false;
+        }
+    }
+    return st.empty();
+}
+
+
+int main()
+{
+    string s="()[{}()]";
+    if(isValid(s))
+    cout<<"True"<<endl;
+    else
+    cout<<"False"<<endl;
+}
+
