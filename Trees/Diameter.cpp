1bd22091eb6a077f6dcdab60adb9d0fce1487af4
FedoraSukant <guptasukant2@gmail.com>
2024-09-23 23:00:43 +0530

    Added bfa for diameter of a bt by calculating the left and right subtree heights and finding the diameter per node to store the diam

---
+++ Trees/Diameter.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// $ Max Diameter of BT - Number of edges between a path
+
+// $ Brute Force Approach
+// $ TC: O(n) | SC: O(n)
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+    public:
+    int diameter = 0;
+    int calculateHeight(Node* root){
+        if(!root) return 0;
+        int lh = calculateHeight(root -> left);
+        int rh = calculateHeight(root -> right);
+        diameter = max(diameter, lh + rh);
+        return 1 + max(lh, rh);
+    }
+    int diameterOfBinaryTree(Node* root) {
+        calculateHeight(root);
+        return diameter;
+    }
+
+
+};
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->left->right->right = new Node(6);
+    root->left->right->right->right = new Node(7);
+
+    Solution solution;
+
+    int diameter = solution.diameterOfBinaryTree(root);
+
+    cout << "The diameter of the binary tree is: " << diameter << endl;
+}
