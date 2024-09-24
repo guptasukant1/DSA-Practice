46633ea2c88d2e6f316df40b5e3d3559bb651327
FedoraSukant <guptasukant2@gmail.com>
2024-09-24 18:45:08 +0530

    Added OA for diameter of bt using bfs and l&r subtree heights and finding and updating the diameter together using recursive approach. Uses local diameter var instead of a local one and reduces steps

--- Trees/Diameter.cpp
+++ Trees/Diameter.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 // $ Max Diameter of BT - Number of edges between a path
 
-// $ Brute Force Approach
-// $ TC: O(n) | SC: O(n)
 struct Node {
     int data;
     Node* left;
@@ -14,6 +12,8 @@ struct Node {
 
 class Solution {
     public:
+    // $ Brute Force Approach [Finding the heights of l&r subtrees while finding the diameter]
+    // $ TC: O(n) | SC: O(n)
     int diameter = 0;
     int calculateHeight(Node* root){
         if(!root) return 0;
@@ -27,7 +27,22 @@ class Solution {
         return diameter;
     }
 
-
+    // $ Optimal Approach [Finding the heights of l&r subtrees and finding the diameter together thus reducing the steps needed to calculate the l&r heights]
+    // * Not much different from BFA but focusses on bottom-top approach and Postorder traversal.
+    // $ TC: O(n) | SC: O(1)
+    int diameterOfBinaryTreeOA(Node* root){
+        int diameter = 0;
+        height(root, diameter);
+        return diameter;
+    }
+    private:
+    int height(Node* root, int &diameter){
+        if(!root) return 0;
+        int lh = height(root -> left, diameter);
+        int rh = height(root -> right, diameter);
+        diameter = max(diameter, lh + rh);
+        return 1 + max(lh, rh);
+    }
 };
 
 int main() {
@@ -42,6 +57,8 @@ int main() {
     Solution solution;
 
     int diameter = solution.diameterOfBinaryTree(root);
-
     cout << "The diameter of the binary tree is: " << diameter << endl;
+
+    int d1 = solution.diameterOfBinaryTreeOA(root);
+    cout << "The diameter of the binary tree is: " << d1 << endl;
 }
